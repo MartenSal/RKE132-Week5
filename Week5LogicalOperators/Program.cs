7e69d454aff92b7ae742faeb516a8e6543051fff
Märten <marten.salong@tktk.ee>
2023-03-29 12:04:15 +0300

    add week5 code

---
+++ Week5LogicalOperators/Program.cs
@@ -0,0 +1,14 @@
+﻿
+//and - &&
+
+//true && true --> true
+//true && false --> false
+//false && true --> false
+//false && false--> false
+
+
+//or - || (pipes)
+//true || true --> true
+//true || false --> true
+//false || true --> true
+// false || false --> false
